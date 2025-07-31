e90221017608f439f26bfb490241c14c4a9398e3
Lucas Corradin <lucascorradin16@gmail.com>
2025-07-31 00:10:49 -0300

    fix: update resource group name to avoid conflicts

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "rg-aprova-ai-prod"
+  default     = "rg-aprova-ai-prod-v2"
 }
 
 variable "location" {
