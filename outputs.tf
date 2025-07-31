f4b652c47bfc89e948b1b53183da89c1eaab0c9e
Lucas Corradin <lucascorradin16@gmail.com>
2025-07-31 18:18:07 -0300

    fix: corrigir mapeamento das VMs e configurações
    - Corrigido os comandos de importação no workflow
    - Ajustado as configurações das VMs para corresponder ao Azure
    - Removido arquivos desnecessários

--- outputs.tf
+++ outputs.tf
@@ -98,16 +98,7 @@ output "ssh_commands" {
   }
 }
 
-# Monitoring
-output "log_analytics_workspace_id" {
-  description = "ID of the Log Analytics workspace"
-  value       = azurerm_log_analytics_workspace.main.id
-}
 
-output "log_analytics_workspace_name" {
-  description = "Name of the Log Analytics workspace"
-  value       = azurerm_log_analytics_workspace.main.name
-}
 
 # Cost Estimation (for Infracost)
 output "estimated_monthly_cost" {
--- virtual-machines.tf
+++ virtual-machines.tf
@@ -33,44 +33,9 @@ resource "azurerm_linux_virtual_machine" "vm_large" {
   tags = var.tags
 }
 
-# VM 2 - Aprova AI 2 (equivalente ao t4g.xlarge)
-resource "azurerm_linux_virtual_machine" "vm_xlarge" {
-  name                = "vm-aprova-ai-2"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  size                = "Standard_B4ms"
-  admin_username      = var.admin_username
-  
-  network_interface_ids = [
-    azurerm_network_interface.vm_xlarge.id,
-  ]
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.ssh_public_key
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
-    disk_size_gb         = 50
-  }
-
-  source_image_reference {
-    publisher = "Debian"
-    offer     = "debian-12"
-    sku       = "12"
-    version   = "latest"
-  }
-  
-  disable_password_authentication = true
-  
-  tags = var.tags
-}
-
-# VM 3 - Aprova AI 3 (equivalente ao t4g.micro)
+# VM 2 - Aprova AI 2 (equivalente ao t4g.micro)
 resource "azurerm_linux_virtual_machine" "vm_micro" {
-  name                = "vm-aprova-ai-3"
+  name                = "vm-aprova-ai-2"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   size                = "Standard_B1s"
@@ -89,6 +54,41 @@ resource "azurerm_linux_virtual_machine" "vm_micro" {
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
     disk_size_gb         = 30
+  }
+
+  source_image_reference {
+    publisher = "Debian"
+    offer     = "debian-12"
+    sku       = "12"
+    version   = "latest"
+  }
+  
+  disable_password_authentication = true
+  
+  tags = var.tags
+}
+
+# VM 3 - Aprova AI 3 (equivalente ao t4g.xlarge)
+resource "azurerm_linux_virtual_machine" "vm_xlarge" {
+  name                = "vm-aprova-ai-3"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B4ms"
+  admin_username      = var.admin_username
+  
+  network_interface_ids = [
+    azurerm_network_interface.vm_xlarge.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+    disk_size_gb         = 50
   }
 
   source_image_reference {
